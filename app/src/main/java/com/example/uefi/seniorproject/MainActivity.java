948a2568f3b04cf2275bb4a6dafae8c2146e8573
PIYACHAT MONGKOLPATTHANAKUL <taeaew.w@outlook.com>
2017-12-21 23:55:21 +0700

    commit xml

--- app/src/main/java/com/example/uefi/seniorproject/MainActivity.java
+++ app/src/main/java/com/example/uefi/seniorproject/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.uefi.seniorproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Hospital {
+
+    public String address;
+    public String phone;
+
+    public Hospital() {
+    }
+
+    public Hospital(String address, String phone) {
+        this.address = address;
+        this.phone = phone;
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Hospital> hos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, HospitalActivity.class);
+        startActivity(intent);
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        final DatabaseReference myRef = database.getReference();
+//        final DatabaseReference HospitalRef = myRef.child("hospitalList");
+
+        // Read from the database
+//        HospitalRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                hos = (ArrayList<Hospital>) dataSnapshot.getValue();
+//                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+//                    String name = (String) messageSnapshot.child("name").getValue();
+//                    String address = (String) messageSnapshot.child("address").getValue();
+//                    Log.d("address = ", address);
+//                }
+
+//                TextView testText = (TextView) findViewById(R.id.test);
+//                Map map = (Map)dataSnapshot.getValue();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//
+//            }
+//        });
+
     }
 }
--- app/src/main/java/com/example/uefi/seniorproject/MapsActivity.java
+++ app/src/main/java/com/example/uefi/seniorproject/MapsActivity.java
@@ -1,30 +1,81 @@
 package com.example.uefi.seniorproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+
+    final LatLng sydney = new LatLng(13.741163702, 100.534331196);
+    final LatLng myHome = new LatLng(13.7373388, 100.56059759999994);
+
+    Button buttonNormal;
+    Button buttonSatellite;
+    Button buttonHybrid;
+    Button buttonNavigate;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        buttonNormal = (Button) findViewById(R.id.buttonNormal);
+        buttonSatellite = (Button) findViewById(R.id.buttonSatellite);
+        buttonHybrid = (Button) findViewById(R.id.buttonHybrid);
+        buttonNavigate = (Button) findViewById(R.id.buttonNavigate);
+
+        buttonNormal.setOnClickListener(this);
+        buttonSatellite.setOnClickListener(this);
+        buttonHybrid.setOnClickListener(this);
+        buttonNavigate.setOnClickListener(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonNavigate:
+                openGoogleMap(myHome, sydney);
+                break;
+            case R.id.buttonSatellite:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.buttonHybrid:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            default:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+        }
 
+        mapFragment.getMapAsync(this);
+    }
+
+    private void openGoogleMap(LatLng src, LatLng dest) {
+        String url = "http://maps.google.com/maps?saddr="+src.latitude+","+src.longitude+"&daddr="+dest.latitude+","+dest.longitude+"&mode=driving";
+        Uri gmmIntentUri = Uri.parse(url);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -39,8 +90,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng sydney = new LatLng(13.741163702, 100.534331196);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").snippet("Hello Sydney"));
+
+        LatLng myHome = new LatLng(13.7373388, 100.56059759999994);
+        mMap.addMarker(new MarkerOptions().position(myHome).title("My Home").snippet("Hello my home"));
+
+//        LatLng center = new LatLng(-34.05, 150.5);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myHome, 100
+        ));
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+
+            @Override
+            // Return null here, so that getInfoContents() is called next.
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Inflate the layouts for the info window, title and snippet.
+                View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+
+
+                TextView title = ((TextView) infoWindow.findViewById(R.id.textViewName));
+                title.setText(marker.getTitle());
+
+
+                TextView snippet = ((TextView) infoWindow.findViewById(R.id.textViewSnippet));
+                snippet.setText(marker.getSnippet());
+
+                ImageView imageView = (ImageView) infoWindow.findViewById(R.id.imageView);
+                imageView.setImageResource(R.drawable.ic_city);
+                if ("My Home".equals(marker.getTitle())) {
+                    imageView.setImageResource(R.drawable.ic_home);
+                }
+
+                return infoWindow;
+            }
+        });
     }
+
 }
